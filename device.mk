2c2f750f538bbceff6266995489f769714229531
songd194 <songd3814@gmail.com>
2026-04-11 20:34:17 +0700

    Update device.mk for DOTOS_CPH1909 configuration

--- device.mk
+++ device.mk
@@ -2,27 +2,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Include vendor
-$(call inherit-product, vendor/oppo/cph1909/vendor.mk)
-
 # Device naming
-PRODUCT_NAME := aosp_cph1909
+PRODUCT_NAME := DOTOS_CPH1909
 PRODUCT_DEVICE := cph1909
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := A5s
-TARGET_SUPPORTS_FACEUNLOCK := false
-WITH_FACEUNLOCK := false
+TARGET_SUPPORTS_FACEUNLOCK := true
+WITH_FACEUNLOCK := true
 
-# Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
-
-# Fingerprint support
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
-
-# Fingerprint HAL
-PRODUCT_PACKAGES += \
-    fingerprint.mt6765 \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
