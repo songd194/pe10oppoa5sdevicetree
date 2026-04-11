ee30a15b1cc578c30f42d603e461cb6c88a2aaf6
songd194 <songd3814@gmail.com>
2026-04-11 20:34:48 +0700

    Update product name and inherit configurations

--- aosp_cph1909.mk
+++ aosp_cph1909.mk
@@ -1,18 +1,7 @@
-# Inherit from device config
-$(call inherit-product, device/oppo/cph1909/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit from PixelExperience common config (CORRECT PATH)
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Product identifiers
-PRODUCT_NAME := aosp_cph1909
+PRODUCT_NAME := dot_cph1909
 PRODUCT_DEVICE := cph1909
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := A5s
-PRODUCT_MANUFACTURER := OPPO
-PRODUCT_SHIPPING_API_LEVEL := 29
-# GApps configuration
-TARGET_GAPPS_ARCH := arm64
-GAPPS_VARIANT := stock
-GAPPS_FORCE_PACKAGE_OVERRIDES := true
-WITH_GAPPS := true
