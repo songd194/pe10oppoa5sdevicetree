cf43491c040c3bb4d1ef6ed6c553102edab92737
songd194 <songd3814@gmail.com>
2026-04-15 20:16:23 +0700

    Update aosp_cph1909.mk

--- aosp_cph1909.mk
+++ aosp_cph1909.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-PRODUCT_NAME := dot_cph1909
+PRODUCT_NAME := havoc_cph1909
 PRODUCT_DEVICE := cph1909
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := A5s
